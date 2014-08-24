2e7e1d6989e82a8d63a14150324272317a91dc57
Andrew Paradise <adparadise@gmail.com>
2014-08-24 17:22:29 -0400

    ensure working for multiple content blocks and javascript

--- tests/slide-reader-test.js
+++ tests/slide-reader-test.js
@@ -23,5 +23,46 @@ describe('SlideReader', function () {
                 done();
             }
         });
+
+        it('should read multiple content blocks', function (done) {
+            var reader, contents;
+            reader = new SlideReader();
+
+            contents = [
+                '/* Main */',
+                '# Welcome',
+                '---',
+                '/* Other */',
+                '# Noice',
+                '---',
+                '{}'
+            ].join('\n');
+            reader.readFromString(contents, callback);
+
+            function callback (error, slide) {
+                assert.equal(error, undefined);
+                assert.equal(slide.getContentBlock('Other'), '<h1>Noice</h1>');
+                done();
+            }
+        });
+
+        it('should read the JavaScript block', function (done) {
+            var reader, contents;
+            reader = new SlideReader();
+
+            contents = [
+                '/* Main */',
+                '# Welcome',
+                '---',
+                '{ config: true }'
+            ].join('\n');
+            reader.readFromString(contents, callback);
+
+            function callback (error, slide) {
+                assert.equal(error, undefined);
+                assert.equal(slide.getJavaScriptBlock(), '{ config: true }');
+                done();
+            }
+        });
     });
 });
