829d57b7e65bcc53d974de345bf8db7d64f58563
Andrew Paradise <adparadise@gmail.com>
2014-08-24 20:57:36 -0400

    basic start to the slides endpoint

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var static = require('node-static');
 var HTTP = require('http');
 var winston = require('winston');
 
+var SlideServer = require('./lib/slide-server');
+
 var port = 8081;
 var fileServer, server;
+var slideServer;
 
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, {
@@ -12,10 +15,15 @@ winston.add(winston.transports.Console, {
 });
 
 fileServer = new static.Server('./public');
+slideServer = new SlideServer('./slides');
 server = HTTP.createServer(serverHandler);
 
 function serverHandler (request, response) {
-    fileServer.serve(request, response);
+    if (request.url.indexOf(slideServer.prefix) === 0) {
+        slideServer.handle(request, response);
+    } else {
+        fileServer.serve(request, response);
+    }
 }
 
 server.listen(port);
